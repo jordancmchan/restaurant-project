84b7357619ce8a0f8eb06750abee6a2b32276d27
jordanchan <jordancmchan@hotmail.com>
2021-04-16 15:12:50 -0400

    added side menu

---
+++ src/Contact.js
@@ -0,0 +1,11 @@
+//Contact.js
+
+const Contact = () => {
+     return(
+          <section class = "contactContainer">
+               <p>Contact and reservations</p>
+          </section>
+     )
+}
+
+export default Contact
---
+++ src/Menu.js
@@ -0,0 +1,11 @@
+//Menu.js
+
+const About = () => {
+     return(
+          <section class="menuContainer">
+               <p>Menu</p>
+          </section>
+     )
+}
+
+export default About
