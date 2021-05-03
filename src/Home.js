0f809c42298ad2b81c0877fd26bcd0250c77f5df
jordancmchan <jordancmchan@hotmail.com>
2021-05-03 01:13:40 -0400

    finished home page

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,6 @@ import galleryTitle from './assets/galleryTitle.jpg'
 import titleImage from './assets/titleImage.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHippo } from "@fortawesome/free-solid-svg-icons";
-import { faSortDown } from "@fortawesome/free-solid-svg-icons";
 import { useState } from 'react';
 import ContactModal from './ContactModal.js';
 import Gallery from './Gallery.js'
@@ -28,22 +27,23 @@ const Home = () => {
                                    <img src={titleImage} alt=""/>
                               </div>
                               <div className="mainTitle">
-                                   <h1>the Hungry Hippo <FontAwesomeIcon icon={faHippo}/></h1>
-                                   <p>Welcome to Hungry Hippo! Inspired by eating good and all the restaurants in Toronto</p>
-                                   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro omnis iusto nobis consequuntur repellendus perspiciatis expedita aliquid delectus velit excepturi rem error quam esse ipsum sapiente, aspernatur eaque atque unde veniam placeat modi. Odit adipisci voluptatum reprehenderit architecto quidem dolore doloribus laborum. Itaque, quidem culpa recusandae in consequatur quasi provident magni eius reprehenderit labore corporis et deserunt, sunt amet eaque!</p>
-                                   <FontAwesomeIcon icon={faSortDown} className="downArrow"/>
-                              </div>
-                         </div>
-                         <div className="contactGalleryContainer" >
-                              <div className="reservationContainer">
-                                   <h1>CONTACT & HOURS</h1>
-                                   <img src={reservationBanner} alt="restaurant table with wine glasses and dinner plates"/>
-                                   <div className="shadow" onClick={handleContactClick}></div>
-                              </div>
-                              <div className="galleryTitleContainer">
-                                   <h1>GALLERY</h1>
-                                   <img src={galleryTitle} alt="a pair of hands kneading dough"/>
-                                   <div className="shadow" onClick = {handleGalleryClick}></div>
+                                   <div className="mainTitleDes">
+                                        <h1>the Hungry Hippo <FontAwesomeIcon icon={faHippo}/></h1>
+                                        <p>Welcome to Hungry Hippo! Inspired by eating good and all the restaurants in Toronto</p>
+                                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Porro omnis iusto nobis consequuntur repellendus perspiciatis expedita aliquid delectus velit excepturi rem error quam esse ipsum sapiente, aspernatur eaque atque unde veniam placeat modi. Odit adipisci voluptatum reprehenderit architecto quidem dolore doloribus laborum. Itaque, quidem culpa recusandae in consequatur quasi provident magni eius reprehenderit labore corporis et deserunt, sunt amet eaque!</p>
+                                   </div>
+                                   <div className="contactGalleryContainer" >
+                                        <div className="reservationContainer">
+                                             <h1>CONTACT & HOURS</h1>
+                                             <img src={reservationBanner} alt="restaurant table with wine glasses and dinner plates"/>
+                                             <div className="shadow" onClick={handleContactClick}></div>
+                                        </div>
+                                        <div className="galleryTitleContainer">
+                                             <h1>GALLERY</h1>
+                                             <img src={galleryTitle} alt="a pair of hands kneading dough"/>
+                                             <div className="shadow" onClick = {handleGalleryClick}></div>
+                                        </div>
+                                   </div>
                               </div>
                          </div>
 
@@ -60,10 +60,6 @@ const Home = () => {
                          />
                          : null
                          }
-
-
-
-
                </section>
 
      )
